cb357500c34c1963fbfcb7a7107e2c06b0fa7271
Hieu Nguyen <nthieu173@gmail.com>
2021-08-23 09:35:44 -0400

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,65 @@
 mod mcts;
 
-fn main() {
-    println!("Hello, world!");
+use disastle_castle_rust::Room;
+use disastle_rust::{
+    game::{player::PlayerInfo, GameSetting, GameState},
+    load_disasters, load_rooms,
+};
+use mcts::{tree_search, ShallowNode};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let disasters = load_disasters(&Path::new("disasters.ron"))?
+        .into_iter()
+        .map(|d| d.to_disaster())
+        .collect();
+    let mut rooms: HashSet<Box<dyn Room>> = load_rooms(&Path::new("rooms.ron"))?
+        .into_iter()
+        .map(|r| r.to_room())
+        .collect();
+    for throne in load_rooms(&Path::new("thrones.ron"))?.into_iter() {
+        rooms.insert(throne.to_room());
+    }
+    let setting = GameSetting {
+        disasters,
+        rooms,
+        num_disasters: 5,
+        num_safe: 15,
+        num_shop: 5,
+    };
+
+    let players = vec![
+        PlayerInfo {
+            name: "one".to_string(),
+            secret: "1".to_string(),
+        },
+        PlayerInfo {
+            name: "two".to_string(),
+            secret: "2".to_string(),
+        },
+        PlayerInfo {
+            name: "three".to_string(),
+            secret: "3".to_string(),
+        },
+        PlayerInfo {
+            name: "four".to_string(),
+            secret: "4".to_string(),
+        },
+    ];
+    let game = GameState::new(&players, setting).schrodinger("1").unwrap();
+    let mut node = ShallowNode {
+        state: game,
+        children: HashMap::new(),
+        num_play: 0.0,
+        num_win: 0.0,
+        player: "1".to_string(),
+    };
+    println!("Node initilization successful");
+    for _ in 0..100 {
+        tree_search(&mut node);
+    }
+    Ok(())
 }
--- src/mcts.rs
+++ src/mcts.rs
@@ -1,14 +1,14 @@
 use disastle_castle_rust::Action;
 use disastle_rust::game::SchrodingerGameState;
-use rand::{thread_rng, Rng};
+use rand::{prelude::IteratorRandom, thread_rng, Rng};
 use std::collections::HashMap;
 
 const EXPLORATION_PARAMETER: f32 = std::f32::consts::SQRT_2;
 const LARGE_NUMBER_FOR_UNEXPLORED: f32 = 1000000.0;
 
-fn tree_search(start_node: &mut Node) {
+pub fn tree_search(start_node: &mut ShallowNode) {
     let mut actions = Vec::new();
-    let mut curr: &Node = start_node;
+    let mut curr: &ShallowNode = start_node;
     loop {
         if let Some(action) = start_node.selection() {
             actions.push(action);
@@ -17,15 +17,15 @@ fn tree_search(start_node: &mut Node) {
             break;
         }
     }
-    drop(curr);
-    let mut leaf: &mut Node = start_node;
+    let mut leaf: &mut ShallowNode = start_node;
     for action in actions.iter() {
         leaf = leaf.children.get_mut(action).unwrap();
     }
-    leaf.expand();
-    if leaf.state.is_victorious(&leaf.player) {
+    leaf = leaf.expand();
+    // Simulation and backpropagation
+    if leaf.simulate() {
         drop(leaf);
-        let mut curr: &mut Node = start_node;
+        let mut curr: &mut ShallowNode = start_node;
         curr.num_win += 1.0;
         curr.num_play += 1.0;
         for action in actions {
@@ -34,7 +34,7 @@ fn tree_search(start_node: &mut Node) {
             curr.num_play += 1.0;
         }
     } else {
-        let mut curr: &mut Node = start_node;
+        let mut curr: &mut ShallowNode = start_node;
         curr.num_play += 1.0;
         for action in actions {
             let mut curr = curr.children.get_mut(&action).unwrap();
@@ -43,15 +43,17 @@ fn tree_search(start_node: &mut Node) {
     }
 }
 
-struct Node {
+// Stop do not save the expansion when a new shop is dealt (randomness is introducted)
+// Then, expand, simulate and backpropagate
+pub struct ShallowNode {
     pub player: String,
     pub state: SchrodingerGameState,
     pub num_win: f32,
     pub num_play: f32,
-    pub children: HashMap<Action, Node>,
+    pub children: HashMap<Action, ShallowNode>,
 }
 
-impl Node {
+impl ShallowNode {
     fn uct_score(&self, parent_num_play: f32) -> f32 {
         if self.num_play == 0.0 {
             return LARGE_NUMBER_FOR_UNEXPLORED
@@ -95,26 +97,54 @@ impl Node {
         }
         Some(max_action.clone())
     }
-    pub fn expand(&mut self) {
-        if self.state.is_over() {
-            self.num_play += 1.0;
-            self.num_win += 1.0;
-        }
-        let turn_player = &self.state.get_turn_index().to_string();
-        for action in self.state.possible_actions(turn_player) {
-            self.children.insert(
-                action,
-                Node {
+    /*
+     * Expand the leaf node and choose one random child to become the new leaf node.
+     * If there are no children (terminal state) or if the expansion would rely on randomness
+     * (the round incremented and the shop was dealt), we immediately backpropagate.
+     */
+    pub fn expand(&mut self) -> &mut ShallowNode {
+        if !self.state.is_over() {
+            let mut is_random = false;
+            let mut new_nodes = Vec::new();
+            let turn_player = &self.state.get_turn_index().to_string();
+            for action in self.state.possible_actions(turn_player) {
+                let new_node = ShallowNode {
                     player: self.player.clone(),
                     state: self.state.action(turn_player, action).unwrap(),
                     num_win: 0.0,
                     num_play: 0.0,
                     children: HashMap::new(),
-                },
-            );
+                };
+                if new_node.state.round == self.state.round {
+                    new_nodes.push((action, new_node));
+                } else {
+                    is_random = true;
+                    break;
+                }
+            }
+            if !is_random {
+                for (action, new_node) in new_nodes {
+                    self.children.insert(action, new_node);
+                }
+                return self
+                    .children
+                    .values_mut()
+                    .choose(&mut thread_rng())
+                    .unwrap();
+            }
         }
+        return self;
     }
-    pub fn evaluation(&self) -> bool {
-        self.state.is_victorious(&self.player)
+    pub fn simulate(&self) -> bool {
+        let mut game = self.state.clone();
+        let mut rng = thread_rng();
+        while !game.is_over() {
+            let turn_player = &game.turn_order[game.turn_index];
+            let actions = self.state.possible_actions(turn_player);
+            game = game
+                .action(turn_player, actions.into_iter().choose(&mut rng).unwrap())
+                .unwrap();
+        }
+        game.is_victorious(&self.player)
     }
 }
